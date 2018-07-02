872470facd0f72c715936fedce3d9e2cdc8f09d9
quincy <kunchi68@gmail.com>
2018-07-03 01:03:03 +0800

    Add copyspecial.py solution

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -17,8 +17,32 @@ import commands
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def get_special_paths(dir):
+    dir_path = os.path.abspath(dir)
+    if not os.path.exists(dir_path) :
+        print "error: dir '" + dir + "' does not exist"
+        sys.exit(1)
 
+    filenames = []
+    for filename in os.listdir(dir_path):
+        if re.search(r'__\w+__', filename) :
+            filenames.append(os.path.join(dir_path, filename))
+    return filenames
 
+def copy_to(paths, dir):
+    dir_path = os.path.abspath(dir)
+    if not os.path.exists(dir_path) :
+        os.makedirs(dir_path)
+    for filename in paths:
+        shutil.copy(filename, dir_path)
+
+def zip_to(paths, zippath):
+    cmd = "zip -j " + zippath + " " + " ".join(paths)
+    print "Command I'm going to do:" + cmd
+    (status, output) = commands.getstatusoutput(cmd)
+    if status:
+        sys.stderr.write(output + "\n")
+        sys.exit(status)
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -50,6 +74,13 @@ def main():
 
   # +++your code here+++
   # Call your functions
+  paths = []
+  for dir in args :
+    files = get_special_paths(dir)
+    paths.extend(files)
+  #print paths
+  if todir : copy_to(paths, todir)
+  if tozip : zip_to(paths, tozip)
   
 if __name__ == "__main__":
   main()
